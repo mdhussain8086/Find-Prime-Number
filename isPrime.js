1bdfede5f9f2f349d51496110695ad98050734e8
hussain <85216698+mdhussain8086@users.noreply.github.com>
2021-11-06 17:48:30 +0530

    Add files via upload

--- isPrime.js
+++ isPrime.js
@@ -1,1 +1,32 @@
-
+// Return number is prime or not
+function checkPrime(num) {
+    if(num <=1) {
+        return false;
+    }else {
+        for(let i = 2; i< num; i++) {
+            if(num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// array containing number
+let arr = [0,1,4,6,8,9,10,13];
+let check = [];
+// Getting each element of array and push to new array that element
+arr.forEach(function(ele) {
+    const isPrime = checkPrime(ele);
+    if(isPrime) {
+        check.push(ele);
+    }
+});
+// Returning that number which is Prime otherwise zero.
+function getNumber() {
+    if(check.length > 0) {
+        return check[0];
+    }else {
+        return 0;
+    }
+}
+console.log(getNumber())
